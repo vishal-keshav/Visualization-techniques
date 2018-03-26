223a2e659aabe0e46a98a65b98fa6e7de919521d
bulletcross <bulletcross@gmail.com>
2018-03-26 20:41:36 +0530

    stripped down to main script

--- image_chroma_visualization/ChannelizeImage.m
+++
@@ -1,5 +0,0 @@
-
-function im_channels = ChannelizeImage(im, mask)
-im_channels = {};
-im_channels{1} = cast(bsxfun(@times, double(im), mask), 'like', im);
-im_channels{2} = cast(MaskedLocalAbsoluteDeviation(im, mask), 'like', im);
